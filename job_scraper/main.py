af5ee35d3d703dc6f04a2dd60d2d032af1e1e5bc
Adam Conrad <adamconrad720@gmail.com>
2024-03-16 12:32:58 -0700

    updated strucutre for modularity

--- job_scraper/main.py
+++ job_scraper/main.py
@@ -1,19 +1,7 @@
 import requests
 import pprint
 import xml.etree.ElementTree as ET
-
-# Parse the XML file
-tree = ET.parse('linkedin_api_calls/logged_in_job_search_unencoded.xml')  
-root = tree.getroot()
-req_list = root[0][8].text.split('\n')
-req_list = list(filter(lambda x: x != '', req_list))
-
-# Parse request from XML
-headers_dict = {}
-for i in req_list[1:]:
-    header_name, header_value = i.split(": ", 1)
-    headers_dict[header_name] = header_value
-url = 'https://' + headers_dict['Host'] + req_list[0].split(' ')[1]
+from utils import get_url
 
 try:
     with open('urns.txt', 'r') as file:
@@ -21,14 +9,12 @@ try:
 except FileNotFoundError:
     collected_urns = set()
 
-
 with open('urns.txt', 'a') as file:
     for i in range(10):
-        count = 0
-        cur_url = url.replace('<PAGE_NUM>', str(i*100))
-        #print(cur_url)
         print('checking page', i)
-        res = requests.get(cur_url, headers=headers_dict)
+        count = 0
+        cur_url, headers = get_url('linkedin_api/logged_in_job_search_unencoded.xml', '<PAGE_NUM>', str(i*100))
+        res = requests.get(cur_url, headers=headers)
         urns = res.json()['data']['metadata']['jobCardPrefetchQueries'][-1]['prefetchJobPostingCardUrns']
         urns = [n.split('(')[-1].split(',')[0] for n in urns if n not in collected_urns]
     
